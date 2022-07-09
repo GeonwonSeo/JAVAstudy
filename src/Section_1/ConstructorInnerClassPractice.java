d5bf3276155c38cdb68041da5d142db9059b611a
GeonwonSeo <pisatan@gmail.com>
2022-07-09 09:07:56 +0900

    InnerClass

--- src/Section_1/ConstructorInnerClassPractice.java
+++ src/Section_1/ConstructorInnerClassPractice.java
@@ -1,6 +1,6 @@
 package Section_1;
 
-public class ConstructorInnerClassPractice {
+class ConstructorInnerClassPractice {
     public static void main(String[] args) {
         Print p1 = new Print("----");
         p1.A();
@@ -21,19 +21,65 @@ public class ConstructorInnerClassPractice {
     }
 }
 class Print {
-    public String delimiter = "";
-    public Print(String delimiter) {
+    String delimiter = "";
+    Print(String delimiter) {
         this.delimiter = delimiter;
     }
-    public void A() {
+    void A() {
         System.out.println(this.delimiter);
         System.out.println("A");
         System.out.println("A");
     }
 
-    public void B() {
+    void B() {
         System.out.println(this.delimiter);
         System.out.println("B");
         System.out.println("B");
     }
+}
+
+class Champion { //클래스정의
+    String name; //변수정의
+    int power;
+    int defense;
+    public static void main(String[] args) {
+        Champion superman = new Champion();
+        Champion batman = new Champion();
+
+        superman.name = "SuperMan";
+        superman.power = 70;
+        superman.defense = 50;
+        superman.powerDown();
+        superman.getAbilityTest();
+
+        batman.name = "batMan";
+        batman.power = 45;
+        batman.defense = 65;
+        batman.powerUp();
+        batman.getAbilityTest();
+
+        Champion ironman = new Champion("ironman", 40,70);
+    }
+    //생성자정의
+    private Champion() {
+    }
+    //this사용예시
+    private  Champion(String name, int power,int defense) {
+        this.name = name;this.power=power;this.defense=defense;
+        this.getAbilityTest();
+    }
+    //메소드 정의
+    private void powerUp() {
+        power += 5;
+    }
+    private void powerDown(){
+        power -= 5;
+    }
+    private int getCurrentPower(){
+        return power;
+    }
+    private void getAbilityTest(){
+        System.out.printf("%s이 %d의 공격력과 %d의 방어력을 가지고 있습니다.\n",name,power,defense);
+    }
+
 }
